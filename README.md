f79862f3601bd5db41e3800e15682a04c1fdfade
eminorhan <eo41@nyu.edu>
2021-02-07 12:34:12 -0500

    separate model def

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Meta-seq2seq with transformers
+# Meta-seq2seq with generic transformers
 
+The code here uses code from Brenden Lake's [meta-seq2seq repo](https://github.com/facebookresearch/meta_seq2seq) to generate episodes.
+
+##
 ... for so I formd them free, and free they must remain
   
   -- *Paradise Lost* (Book III)
--- scratch.py
+++ scratch.py
@@ -5,11 +5,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import generate_episode as ge
+from models import TransformerModel
 from copy import deepcopy, copy
 
+# CONSTANTS
 USE_CUDA = torch.cuda.is_available()
-
-# Special symbols
 SOS_token = "SOS"  # start of sentence
 EOS_token = "EOS"  # end of sentence
 PAD_token = SOS_token  # padding symbol
@@ -193,73 +193,6 @@ def generate_prim_permutation(shuffle, nsupport, nquery, input_lang, output_lang
 
     return build_sample(x_support, y_support, x_query, y_query, input_lang, output_lang, identifier)
             
-# Temporarily leave this module here. Will be moved somewhere else.
-class PositionalEncoding(nn.Module):
-    r"""Inject some information about the relative or absolute position of the tokens
-        in the sequence. The positional encodings have the same dimension as
-        the embeddings, so that the two can be summed. Here, we use sine and cosine
-        functions of different frequencies.
-    .. math::
-        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
-        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
-        \text{where pos is the word position and i is the embed idx)
-    Args:
-        d_model: the embed dim (required).
-        dropout: the dropout value (default=0.1).
-        max_len: the max. length of the incoming sequence (default=5000).
-    Examples:
-        >>> pos_encoder = PositionalEncoding(d_model)
-    """
-
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        r"""Inputs of forward function
-        Args:
-            x: the sequence fed to the positional encoder model (required).
-        Shape:
-            x: [sequence length, batch size, embed dim]
-            output: [sequence length, batch size, embed dim]
-        Examples:
-            >>> output = pos_encoder(x)
-        """
-
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
-# Temporarily leave this module here. Will be moved somewhere else.
-class TransformerModel(nn.Module):
-    def __init__(self, ntoken, emsize, nhead, nhid, nlayers, dropout=0.1):
-        super(TransformerModel, self).__init__()
-
-        self.ntoken = ntoken
-        self.emsize = emsize
-        self.nhead = nhead
-        self.nhid = nhid
-        self.nlayers = nlayers
-        self.dropout = dropout
-
-        self.embedding = nn.Embedding(self.ntoken, self.emsize)
-        self.pos_encoder = PositionalEncoding(self.emsize, self.dropout)
-        self.transformer =  nn.Transformer(d_model=self.emsize, nhead=self.nhead, num_encoder_layers=self.nlayers, num_decoder_layers=self.nlayers)
-        self.linear = nn.Linear(self.emsize, self.ntoken)
-
-    def forward(self, src, tgt, src_mask=None, tgt_mask=None):    
-        src = self.pos_encoder(self.embedding(src))
-        tgt = self.pos_encoder(self.embedding(tgt))
-        out = self.linear(self.transformer(src, tgt))
-        out = F.log_softmax(out, dim=-1)
-        return out
 
 if __name__ == '__main__':
 
@@ -323,10 +256,10 @@ if __name__ == '__main__':
         samples_val.append(sample)
         tabu_episodes = tabu_update(tabu_episodes, sample['identifier'])
 
+    episode_start_time = time.time()
+
     # start training
     for episode in range(1, args.num_episodes+1):
-
-        episode_start_time = time.time()
 
         model.train()
 
@@ -380,3 +313,5 @@ if __name__ == '__main__':
                 print('-' * 89)
                 print('| iteration {:6d} | time/iter: {:5.2f}s | validation loss {:5.4f} | '.format(episode, (time.time() - episode_start_time) / args.eval_interval, val_loss.item()))
                 print('-' * 89)
+
+                episode_start_time = time.time()
